08802a82221baf946c31901be0fb2bdf78c2302b
Thomas van den Bighelaar <tmsvdb80@gmail.com>
2019-01-16 16:51:19 +0100

    update

--- serial_reader/src/main.rs
+++ serial_reader/src/main.rs
@@ -18,7 +18,7 @@ const DS: u8 = 14;
 const CLCK: u8 = 15;
 const LTCH_IN: u8 = 18;
 const LTCH_OUT: u8 = 23; ///Request data;
-const TIMEOUT: u32 = 1000;
+const TIMEOUT: u128 = 1000;
 
 fn main() {
     let device_info = DeviceInfo::new().unwrap();
